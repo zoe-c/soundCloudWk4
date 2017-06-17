88b6769433782878b00cd6f021615c1780b3c497
zoe-c <find.zoec@gmail.com>
2017-06-17 12:45:22 -0400

    getting there

--- main.js
+++ main.js
@@ -4,123 +4,77 @@ console.log("hola!");
 */
 
 // 1. First select and store the elements you'll be working with
-
-
 // 2. Create your `onSubmit` event for getting the user's search term
-
-
 // 3. Create your `fetch` request that is called after a submission
-
-
 // 4. Create a way to append the fetch results to your page
-
-
 // 5. Create a way to listen for a click that will play the song in the audio play
 
+// Form Submission
+// You should use the onSubmit() method on your form. This will trap when the submit button was pressed, thus allowing you to write a handler function.
+// You'll also need to use the .value on the input to get the current value after the submission has happened.
+// Fetching Data
+// You'll need to take the value from above and use that to build out your URL to send to SoundCloud. (don't forgot to send your API token as well)
 
 
 //ELEMENTS INTO VARIABLES
 var audio = document.getElementById('audio');
 var searchForm = document.getElementById('searchForm');
-var searchText = document.getElementById('searchText');
+var searchInput = document.getElementById('searchInput');
 var submitButton = document.getElementById('submitButton');
 var searchResults = document.getElementById('searchResults');
-// var fetchAddress = "https://api.soundcloud.com/tracks/?client_id=86b6a66bb2d863f5d64dd8a91cd8de94&q=";
-var addToFetch = '';
-
+// var fetchAddress = "";
+var addToFetch = "";
 
 console.log(audio);
 console.log(searchForm);
-console.log(searchText);
+console.log(searchInput);
 console.log(submitButton);
 console.log(searchResults);
 
-
-
-
-
-
-
-submitButton.addEventListener('click', function (event){
-   addToFetch = searchText.value.toString();
+   //submit event
+submitButton.addEventListener('click', function (event) {
+   addToFetch = searchInput.value;
    console.log(addToFetch);
 
-   let fetchAddress = "https://api.soundcloud.com/tracks/?client_id=86b6a66bb2d863f5d64dd8a91cd8de94&q=" + addToFetch;
-   console.log(fetchAddress);
-
-  fetch(fetchAddress)
-   .then(
-      function(response){
-      if (response.status != 200) {
-         console.log("NOT IN MY YARD: ERROR: " + response.status);
-         return;
-      }
-
-      response.json().then(function(data) {
-         console.log(data);
-
-         let tracks = data.results;
-         console.log(tracks);
-
-         function renderTracks() {
-            // let track = tracks[0];
-            for (track of tracks)
-            return `
-               <ul class="tracks"
-
-
-            `
-         }
-
-
-
-
-
-
-
-
-
-         // //assign results to customers
-         // let customers = data.results;
-         // console.log(customers);
-
-         // //create a function to pull "cus"
-         // function renderCustomers(){
-         //    // let customer = customers[0];
-         //    // for (i = 0; i <= customers.length-1; i++) {
-         //    return `
-         //    <div class= "customers">
-         //      ${customers.map(customer =>
-         //          `<ul>
-         //             <li><img src="${customer.picture.large}" alt="customer-photo"></li>
-         //             <li class= "customer-name">${customer.name.first} ${customer.name.last}</li>
-         //             <li class= "customer-email">${customer.email}</li>
-         //             <li class= "address">
-         //               <p>${customer.location.street}</p>
-         //               <p>${customer.location.city} , ${customer.location.state} ${customer.location.postcode}</p>
-         //            </li>
-         //            <li class= "phone">${customer.phone}</li>
-         //         </ul>`
-         //       ).join('')}
-         //    </div>
-         //    `;
-         // }
-         // console.log(renderCustomers());
-         //
-         // let markup = `
-         //      <div class= "title"> INTERNAL COMPANY DIRECTORY </div>
-         //       ${renderCustomers()}
-         //      `
-         // document.body.innerHTML = markup;
-
-      });
-
-
-   })
-   .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
-
-
-
+   //input cleanup still in event
+   // function cleanUp(str) {
+   //    return .split('').map(function(characters) {
+   //       var replacement = addToFetch[characters.toLowerCase()];
+   //       return replacement || characters;
+   //    }).join('');
+   // }
+   //
+   // function cleanUp() {
+   //    addToFetch.split('').map(addToFetch[i].toLowerCase());
+   // }
+   //
+   // addToFetch = cleanup()
 });
+
+// fetchAddress = "https://api.soundcloud.com/tracks/?client_id=86b6a66bb2d863f5d64dd8a91cd8de94&q=" + addToFetch;
+
+fetch('https://api.soundcloud.com/tracks/?client_id=86b6a66bb2d863f5d64dd8a91cd8de94&q=' + addToFetch)
+  .then(
+     function(response){
+        if (response.status != 200) {
+           console.log("WHOA! Error: " + response.status);
+           return;
+         }
+         
+         response.json().then(function(data) {
+          console.log("Here is the data: " + data);
+          console.log(data.results);
+
+          //assign results to tracks
+          let tracks = data.results;
+          console.log(tracks);
+
+
+         });
+
+
+      }
+   )
+  .catch(function(err) {
+   console.log('Fetch Error :-S', err);
+ });
