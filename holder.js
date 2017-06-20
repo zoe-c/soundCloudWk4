f192a59c06e6d14cb4c6dcd88c2bdb58bade0d7e
zoe-c <find.zoec@gmail.com>
2017-06-20 17:55:57 -0400

    audio linked. clean code up before finalize. get on styles.

--- holder.js
+++ holder.js
@@ -23,19 +23,15 @@ submitButton.addEventListener('click', function e(event) {
                }
 
                response.json().then(function(data) {
-                  // console.log(data);
-
                   let tracks = data;
-                  // console.log(tracks);
-
                   var clientId = "/?client_id=86b6a66bb2d863f5d64dd8a91cd8de94";
                   function renderTracks() {
                      return `
                         ${tracks.map(track =>
                            `<ul class="track">
                             <li>
-                              <button class="audioTrigger" type="button" name="${track.stream_url}${clientId}">
-                                 <img src="${track.artwork_url}" alt="oops!-caught-in-the-cloud">
+                              <button class="audioTrigger" type="button">
+                                 <img src="${track.artwork_url}" alt="oops!-caught-in-the-cloud" id="${track.stream_url}${clientId}">
                               </button>
                             </li>
                             <li>${track.title}</li>
@@ -44,71 +40,18 @@ submitButton.addEventListener('click', function e(event) {
                             `).join('')}
                      `;
                   }
-                  // console.log(renderTracks());
 
                   let markup = `${renderTracks()}`;
                   document.getElementById('searchResults').innerHTML = markup;
 
 
-            //Make sure triggers are working// stored in triggers array.
-                  var triggers = document.getElementsByClassName('audioTrigger');
-                  console.log(triggers);
-
-
-            //FIND A WAY TO ITERATE OVER THE ARRAY AND GET THE BUTTON TO ANNOUNCE WHICH INDEX IT IS...OR TO PRINT THE EXACT TRACK's NAME ATTRIBUTE (WHERE YOU'VE STORED THE CORRECTLY FORMATTED STREAM URL FOR YOUR AUDIO) (l-104)
                   var parent = document.getElementById('bodyWrap').addEventListener('click', function (event) {
+                     var triggers = document.getElementsByClassName('audioTrigger');
                      event.target = triggers;
-                     console.log("getting closer!");
-
-                     var audioSRC = document.getElementById('audio').getAttribute("src");
-                     console.log(audioSRC);
-
-                     // // for (var i = 0; i <= triggers.length - 1; i++) {
-                     //    if (event.target ===  && event.target.class === 'audioTrigger') {
-                     //       // event.target = triggers[i];
-                     //       return event.target.name;
-                     //    }
-                     //    console.log(event.target.name);
-
-
-                     //****************************************ATTEMPT***************************************
-                     // var audioWrap = document.getElementById('audioWrap');
-                     // triggers.map(trigger => trigger.addEventListener('click', function (event) {
-                     //    event.target = audioWrap;
-                     //    var newAudio = `<audio src="${track.stream_url}${clientId}" controls= "controls"></audio>`
-                     //    audioWrap.prepend(newAudio);
-                     //
-                     // }));
-
-                     // function audioLink(){
-                     //    var audio = document.getElementById('audio').setAttribute("src", triggers.name);
-                     //    console.log(audio.src);
-                     // }
-
+                     let playTrack = `<audio src="${event.target.id}" id="audio" controls="controls"></audio>`
+                     return document.getElementById('audioWrap').innerHTML = playTrack
 
                   });
-
-
-                  // function audioConnect() {
-                  //    var audio = document.getElementById('audio');
-                  //    var audioTrigger = document.getElementsByClassName('audioTrigger');
-                  //    audioTrigger.addEventListener('click', function (event) {
-                  //       if (e.target.class === "audioTrigger") {
-                  //          // audio.src = "https://api.soundcloud.com/tracks/" + ${track.id} + "/stream/?client_id=86b6a66bb2d863f5d64dd8a91cd8de94";
-                  //          // audio.src = "https://api.soundcloud.com/tracks/" + e.target.id + "/stream/?client_id=86b6a66bb2d863f5d64dd8a91cd8de94" ;
-                  //          // audio.src =  track.stream_url + client_id;
-                  //          // audio.src += e.target.id + clientId;
-                  //          let newSource = "https://api.soundcloud.com/tracks/" + e.target.id + clientIdForStream;
-                  //          // audio.setAttribute("src", newSource);
-                  //          audio.src = newSource;
-                  //       }
-                  //
-                  //       console.log(audio.src)
-                  //
-                  //    })
-                  //
-                  //
-                  // }
 
 
                });
